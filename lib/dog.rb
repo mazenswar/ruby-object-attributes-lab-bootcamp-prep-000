baaba38503779b09bb569d25d6c4028a33606c34
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-21 22:14:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,8 +7,13 @@ class Dog
   def name
     @name
   end 
+  
   def name=(new_name)
     @name = name_name
+  end
+  
+  def breed
+    @breed
   end
   def breed=(new_breed)
     @breed = new_breed
